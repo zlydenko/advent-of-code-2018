6d42ecbd21de6d9a1ebe24a71eefeced00d7a091
anton zlydenko <anton.zlydenko@gmail.com>
2019-03-31 20:44:37 +0300

    add findLikelyPairsId fn

--- day-two/src/__tests__/solution-pt2.spec.ts
+++ day-two/src/__tests__/solution-pt2.spec.ts
@@ -1,7 +1,19 @@
-import { differedLetters, findCorrectBoxesId, commonLetters } from "../solution-pt2";
+import { findLikelyPairsId, differedLetters, findCorrectBoxesId, commonLetters } from "../solution-pt2";
 
 describe("inventory management system pt.2 testcases", () => {
   const testData = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"];
+
+  test("must find likely pair with 2 differ characters", () => {
+    const entryData = findLikelyPairsId(testData[0], 0, [testData[0], testData[4], testData[5]]);
+    const expectedResult = [[testData[0], testData[5], 2]];
+    expect(entryData).toEqual(expectedResult);
+  });
+
+  test("must find likely pair with 1 differ character", () => {
+    const entryData = findLikelyPairsId(testData[1], 0, [testData[1], testData[2], testData[3], testData[4]]);
+    const expectedResult = [[testData[1], testData[4], 1]];
+    expect(entryData).toEqual(expectedResult);
+  });
 
   test("all letters isnt match", () => {
     expect(differedLetters(testData[0], testData[1])).toBe(5);
--- day-two/src/solution-pt2.ts
+++ day-two/src/solution-pt2.ts
@@ -7,7 +7,35 @@ export const differedLetters = (first: string, second: string): number => {
   return differCharacters.length;
 };
 
+export const findLikelyPairsId = (testingId: string, testingIdIdx: number, allIds: string[]) => {
+  return [
+    ...allIds
+      .map((comparingId: string, comparingIdIdx: number) => {
+        if (comparingIdIdx === testingIdIdx) return null;
+
+        const difference = differedLetters(testingId, comparingId);
+        const approvedDiffer = difference <= testingId.length - 1;
+        return approvedDiffer ? [testingId, comparingId, difference] : null;
+      })
+      .filter(value => value !== null)
+  ];
+};
+
 export const findCorrectBoxesId = (ids: string[]): [string, string] => {
+  let likelyList: Array<[string, string, number]> = [];
+
+  ids.forEach((id: string, idx: number) => {
+    const likelyPairs = ids
+      .map((comparingId: string, comparingIdIdx: number) => {
+        if (comparingIdIdx === idx) return null;
+
+        const similarity = differedLetters(id, comparingId);
+
+        return similarity >= 1 ? [id, comparingId, similarity] : null;
+      })
+      .filter(value => value !== null);
+  });
+
   return ["hello", "world"];
 };
 
