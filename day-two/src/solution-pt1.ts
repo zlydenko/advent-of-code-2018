7438403458bf189511dbb978142c9fe1f9b85984
anton zlydenko <anton.zlydenko@gmail.com>
2019-03-31 19:21:27 +0300

    scanning box id fn

--- day-two/src/solution-pt1.ts
+++ day-two/src/solution-pt1.ts
@@ -1,5 +1,27 @@
 import inputData from "./inputLoader";
 
-export const scanBoxId = () => {};
+export const scanBoxId = (id: string): [number, number] => {
+  const splittedId = id.split("");
+  let result: [number, number] = [0, 0];
 
-export const boxIdChecksum = () => {};
+  splittedId.forEach((letter: string) => {
+    if (result[0] === 1 && result[1] === 1) return;
+
+    const duplicates = splittedId.filter((n: string) => n === letter);
+
+    switch (duplicates.length) {
+      case 3:
+        result[1] = 1;
+        break;
+      case 2:
+        result[0] = 1;
+        break;
+    }
+  });
+
+  return result;
+};
+
+export const boxIdChecksum = (input: string[]): number => {
+  return 12;
+};
