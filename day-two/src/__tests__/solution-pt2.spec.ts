e8043608189e6e9472146bdd01e65f12265acacd
anton zlydenko <anton.zlydenko@gmail.com>
2019-04-06 20:20:19 +0300

    fresh start :tada:

--- day-two/src/__tests__/solution-pt2.spec.ts
+++ day-two/src/__tests__/solution-pt2.spec.ts
@@ -1,47 +1,61 @@
-import { findSolution, findDifferChars, sliceDiffer, findCorrectBoxIds } from "../solution-pt2";
+// import { findSolution, findDifferChars, sliceDiffer, findCorrectBoxIds } from "../solution-pt2";
 import inputData from "../inputLoader";
+
+import { findDifferCount } from "../solution-pt2";
 
 describe("inventory management system pt.2 testcases", () => {
   const testData = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"];
 
-  test("it must find differ characters", () => {
-    const first = testData[0];
-    const second = testData[5];
-    const differ = findDifferChars(first, second);
-    const output = ["x", "y", "b", "d"];
-
-    expect(differ).toEqual(expect.arrayContaining(output));
+  test("it must output how many characters is not equal (examples)", () => {
+    expect(findDifferCount(testData[0], testData[1])).toBe(5);
+    expect(findDifferCount(testData[0], testData[5])).toBe(2);
+    expect(findDifferCount(testData[1], testData[4])).toBe(1);
   });
 
-  test("it must slice differ chars", () => {
-    const chars = ["x", "y", "b", "d"];
-    const firstInput = testData[0];
-    const secondInput = testData[5];
-
-    const firstOutput = "ace";
-    const secondOutput = "ace";
-
-    expect(sliceDiffer(firstInput, chars)).toBe(firstOutput);
-    expect(sliceDiffer(secondInput, chars)).toBe(secondOutput);
+  test("it must output how many characters is not equal (real data)", async () => {
+    const realData = await inputData();
+    expect(findDifferCount(realData[0], realData[1])).toBe(6);
+    expect(findDifferCount(realData[1], realData[2])).toBe(6);
   });
 
-  test("find correct box ids", () => {
-    const input = testData;
-    const output = ["fguij", "fghij"];
-    const result = findCorrectBoxIds(input);
+  // test("it must find differ characters", () => {
+  //   const first = testData[0];
+  //   const second = testData[5];
+  //   const differ = findDifferChars(first, second);
+  //   const output = ["x", "y", "b", "d"];
 
-    expect(result).toEqual(expect.arrayContaining(output));
-  });
+  //   expect(differ).toEqual(expect.arrayContaining(output));
+  // });
 
-  test("it must provide valid solution", async () => {
-    const data = await inputData();
-    const solution = findSolution(data);
+  // test("it must slice differ chars", () => {
+  //   const chars = ["x", "y", "b", "d"];
+  //   const firstInput = testData[0];
+  //   const secondInput = testData[5];
 
-    console.log(solution.ids);
-    console.log(solution.commonCharacters);
+  //   const firstOutput = "ace";
+  //   const secondOutput = "ace";
 
-    expect(solution).toHaveProperty("ids");
-    expect(solution).toHaveProperty("commonCharacters");
-    expect(solution.ids).toHaveLength(2);
-  });
+  //   expect(sliceDiffer(firstInput, chars)).toBe(firstOutput);
+  //   expect(sliceDiffer(secondInput, chars)).toBe(secondOutput);
+  // });
+
+  // test("find correct box ids", () => {
+  //   const input = testData;
+  //   const output = ["fguij", "fghij"];
+  //   const result = findCorrectBoxIds(input);
+
+  //   expect(result).toEqual(expect.arrayContaining(output));
+  // });
+
+  // test("it must provide valid solution", async () => {
+  //   const data = await inputData();
+  //   const solution = findSolution(data);
+
+  //   console.log(solution.ids);
+  //   console.log(solution.commonCharacters);
+
+  //   expect(solution).toHaveProperty("ids");
+  //   expect(solution).toHaveProperty("commonCharacters");
+  //   expect(solution.ids).toHaveLength(2);
+  // });
 });
--- day-two/src/solution-pt2.ts
+++ day-two/src/solution-pt2.ts
@@ -1,69 +1,14 @@
-function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
-  return value !== null && value !== undefined;
-}
-
-export const findDifferChars = (first: string, second: string): string[] => {
-  const firstLetters: string[] = first.split("");
-  const secondLetters: string[] = second.split("");
-
-  const allUnique: Set<string> = new Set([...firstLetters, ...secondLetters]);
-
-  let result: string[] = [];
-
-  allUnique.forEach(value => {
-    if (
-      (firstLetters.includes(value) && !secondLetters.includes(value)) ||
-      (!firstLetters.includes(value) && secondLetters.includes(value))
-    ) {
-      result.push(value);
-    }
-  });
-
-  return result;
+const findCommonLetters = (first: string[], second: string[]): string[] => {
+  return first.reduce((acc: string[], current: string, idx: number) => {
+    const isCommon: boolean = second[idx] === current;
+    return isCommon ? [...acc, current] : acc;
+  }, []);
 };
 
-export const sliceDiffer = (input: string, chars: string[]): string => {
-  return input
-    .split("")
-    .filter(letter => !chars.includes(letter))
-    .join("");
-};
+export const findDifferCount = (first: string, second: string): number => {
+  const [firstArr, secondArr]: [string[], string[]] = [first.split(""), second.split("")];
 
-interface Answer {
-  ids: string[];
-  commonCharacters: string;
-}
+  const commonLetters = findCommonLetters(firstArr, secondArr);
 
-export const findCorrectBoxIds = (ids: string[]): string[] => {
-  let result: string[] = [];
-
-  ids.forEach((currentId: string, idx: number, allIds: string[]) => {
-    const testingIds = allIds.slice(idx);
-    const correctBoxIds: string[] = testingIds.reduce((acc: string[], testedId: string) => {
-      console.log(`testing value ${currentId} and ${testedId}`);
-      const differChars = findDifferChars(currentId, testedId);
-      console.log(`count of differ chars: ${differChars.length / 2}`);
-      const isValid = differChars.length / 2 === 1;
-      return isValid ? [currentId, testedId] : acc;
-    }, []);
-
-    if (correctBoxIds.length > 0) {
-      result = correctBoxIds;
-      console.log("result found, breaking loop");
-      return;
-    }
-  });
-
-  return result;
-};
-
-export const findSolution = (data: string[]): Answer => {
-  const [firstId, secondId] = findCorrectBoxIds(data);
-  const differCharacters = findDifferChars(firstId, secondId);
-  const commonCharacters = sliceDiffer(firstId, differCharacters);
-
-  return {
-    ids: [firstId, secondId],
-    commonCharacters
-  };
+  return first.length - commonLetters.length;
 };
