45a94504113584cae91c7e888ee5bb785e618aed
anton zlydenko <anton.zlydenko@gmail.com>
2019-04-07 13:22:45 +0300

    almost done (untested on real data)

--- day-three/src/__tests__/solution-pt1.spec.ts
+++ day-three/src/__tests__/solution-pt1.spec.ts
@@ -1,4 +1,4 @@
-import { parseFabricSlice } from "../solution-pt1";
+import { parseFabricSlice, findOverlap, getOverlapSize } from "../solution-pt1";
 
 describe("No Matter How You Slice It pt.1 testcases", () => {
   test("it must provide valid parsed data of slice", () => {
@@ -28,5 +28,45 @@ describe("No Matter How You Slice It pt.1 testcases", () => {
     const result = parseFabricSlice(input);
     expect(result.id).toBe(123);
     expect(result.coords).toEqual(expect.arrayContaining(expected.coords));
+  });
+
+  test("it must provide valid overlap coords", () => {
+    const testData = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"];
+    const firstSlice = parseFabricSlice(testData[0]);
+    const secondSlice = parseFabricSlice(testData[1]);
+    const thirdSlice = parseFabricSlice(testData[2]);
+    const overlapCoords = findOverlap([firstSlice, secondSlice, thirdSlice]);
+    const expectedOutput = [
+      {
+        x: 4,
+        y: 4
+      },
+      {
+        x: 5,
+        y: 4
+      },
+      {
+        x: 4,
+        y: 5
+      },
+      {
+        x: 5,
+        y: 5
+      }
+    ];
+
+    expect(overlapCoords).toEqual(expect.arrayContaining(expectedOutput));
+  });
+
+  test("it must provide how many overlaped square inches", () => {
+    const testData = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"];
+    const firstSlice = parseFabricSlice(testData[0]);
+    const secondSlice = parseFabricSlice(testData[1]);
+    const thirdSlice = parseFabricSlice(testData[2]);
+    const overlapCoords = findOverlap([firstSlice, secondSlice, thirdSlice]);
+    const overlapSize = getOverlapSize(overlapCoords);
+    const output = 4;
+
+    expect(overlapSize).toBe(output);
   });
 });
--- day-three/src/solution-pt1.ts
+++ day-three/src/solution-pt1.ts
@@ -18,12 +18,6 @@ export const parseFabricSlice = (input: string): Slice => {
     x: +startPointsStr[0] + 1,
     y: +startPointsStr[1].replace(":", "") + 1
   };
-  // const sliceSize: number = sizes.split("x").reduce((acc: number, value: string) => acc * +value, 1);
-  // const coords = Array(sliceSize)
-  //   .fill(undefined)
-  //   .map((_: undefined, idx: number) => {
-
-  //   });
   const [height, width] = sizes.split("x");
   const coords: Coordinates[] = Array(+height)
     .fill(undefined)
@@ -51,4 +45,43 @@ export const parseFabricSlice = (input: string): Slice => {
     id: sliceId,
     coords
   };
+};
+
+export const findOverlap = (parsedData: Slice[]): Coordinates[] => {
+  const coordsMap: Map<string, number> = parsedData.reduce((acc: Map<string, number>, value: Slice) => {
+    value.coords.forEach((val: Coordinates) => {
+      const key = `${val.x},${val.y}`;
+
+      if (acc.has(key)) {
+        const val = acc.get(key);
+        //? i must do it because of typescript warning
+        //? ts thinks that after i checked map has key value
+        //? it still can be undefined .____.
+
+        const counter = val === undefined ? 0 : val;
+        acc.set(key, counter + 1);
+      } else {
+        acc.set(key, 1);
+      }
+    });
+    return acc;
+  }, new Map());
+
+  //TODO: filter those, who have counter 1
+  coordsMap.forEach((value: number, key: string) => {
+    if (value < 2) {
+      coordsMap.delete(key);
+    }
+  });
+
+  const result: Coordinates[] = Array.from(coordsMap).map(([key, _]) => {
+    const [x, y] = key.split(",");
+    return { x: +x, y: +y };
+  });
+
+  return result;
+};
+
+export const getOverlapSize = (overlapCoords: Coordinates[]) => {
+  return overlapCoords.length;
 };
