69f80aa5b18ba4340d961c3d7b7a9024a5b58c8c
anton zlydenko <anton.zlydenko@gmail.com>
2019-05-04 23:37:51 +0300

    find neighbours of coordinates

--- day-six/src/__tests__/solution-pt1.spec.ts
+++ day-six/src/__tests__/solution-pt1.spec.ts
@@ -1,8 +1,14 @@
 import inputLoader from "../inputLoader";
-import { getMaxCoordValue } from "../solution-pt1";
+import {
+  getMaxCoordValue,
+  findNeighbours
+  // checkIfAreaIsInfinite
+  // getManhattanDistance
+} from "../solution-pt1";
 
 describe("day six", () => {
   let data: string[][] = [];
+  let testData = [["1", "1"], ["1", "6"], ["8", "3"], ["3", "4"], ["5", "5"], ["8", "9"]];
 
   beforeAll(async () => {
     data = await inputLoader();
@@ -13,9 +19,48 @@ describe("day six", () => {
   });
 
   test("it must get border size", () => {
-    const output = getMaxCoordValue(data);
-    const expected = 359;
+    const output = getMaxCoordValue(testData);
+    const expected = 9;
 
     expect(output).toBe(expected);
   });
+
+  test("it must get a neighbours", () => {
+    const output = findNeighbours(
+      {
+        x: 1,
+        y: 1
+      },
+      {
+        x: 9,
+        y: 9
+      }
+    );
+    const expected = [
+      {
+        x: 0,
+        y: 1
+      },
+      {
+        x: 2,
+        y: 1
+      },
+      {
+        x: 1,
+        y: 0
+      },
+      {
+        x: 1,
+        y: 2
+      }
+    ];
+
+    expect(output).toEqual(expect.arrayContaining(expected));
+  });
+
+  // test("it must generate Manhattan distance for location", () => {
+  //   const output = getManhattanDistance(testData[0]);
+
+  //   const expectedAreaDist1 = [[0, 1], [2, 1], [1, 0], [1, 2]];
+  //   const expectedAreaDist4 = [[0, 4]];
 });
--- day-six/src/solution-pt1.ts
+++ day-six/src/solution-pt1.ts
@@ -1,8 +1,32 @@
 export const getMaxCoordValue = (input: string[][]): number => {
-  const reducer = (acc: number, val: string[]) => {
-    return acc > +val[0] ? (acc > +val[1] ? acc : +val[1]) : +val[0];
-  };
-  const result = input.reduce(reducer, 0);
-
-  return result;
+  return input
+    .flat()
+    .map(value => +value)
+    .sort((a, b) => b - a)[0];
 };
+
+//todo: it can be better
+export const findNeighbours = (location: coords, max: coords): coords[] => {
+  const { x, y } = location;
+  const isValid = (c: coords): boolean => c.x >= 0 && c.y >= 0 && c.x <= max.x && c.y <= max.y;
+  const result = [{ x: x - 1, y }, { x: x + 1, y }, { x, y: y - 1 }, { x, y: y + 1 }];
+
+  return result.filter((c: coords) => isValid(c));
+};
+
+//todo area coordinates by manhattan distance generator
+
+type coords = {
+  x: number;
+  y: number;
+};
+
+// const areaGenerator = function*(borderPoint: coords, originPoint: coords): IterableIterator<coords[]> {
+//   let distance = 1;
+//   let freeSpaceOver = false;
+
+//   while(!freeSpaceOver) {
+//     // yield [{ x: 1, y: 2 }];
+
+//   }
+// };
