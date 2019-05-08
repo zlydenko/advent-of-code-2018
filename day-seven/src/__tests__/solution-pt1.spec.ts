00c1d3847cfaca428d3f9da2cc4e0e36dbc6c79f
anton <anton.zlydenko@gmail.com>
2019-05-08 11:22:17 +0300

    trash: delete unnecessary code

--- day-seven/src/__tests__/solution-pt1.spec.ts
+++ day-seven/src/__tests__/solution-pt1.spec.ts
@@ -1,7 +1,7 @@
 import inputLoader from '../inputLoader';
-import { parseInput, sortInput } from '../solution-pt1';
+import { parseInput } from '../solution-pt1';
 
-import { Node, Instructions } from '../types';
+// import { Node, Instructions } from '../types';
 
 describe('day 7: part one', () => {
   const testData = [
@@ -31,15 +31,5 @@ describe('day 7: part one', () => {
     const expected = ['CA', 'CF', 'AB'];
 
     expect(output).toEqual(expect.arrayContaining(expected));
-  });
-
-  test('sorting parsed data', () => {
-    const parsedData = parseInput(testData);
-    const output = sortInput(parsedData);
-    const expected = ['AB', 'AD', 'BE', 'CA', 'CF', 'DE', 'FE'];
-
-    expect(output[0]).toBe(expected[0]);
-    expect(output[2]).toBe(expected[2]);
-    expect(output[6]).toBe(expected[6]);
   });
 });
