e8acacd29137a68546c3cccd157e41feb310c81f
anton zlydenko <anton.zlydenko@gmail.com>
2019-05-09 20:59:03 +0300

    solution output was incorrect

--- day-seven/src/__tests__/solution-pt1.spec.ts
+++ day-seven/src/__tests__/solution-pt1.spec.ts
@@ -1,5 +1,5 @@
 import inputLoader from "../inputLoader";
-import { parseInput, createGraph } from "../solution-pt1";
+import { parseInput } from "../solution-pt1";
 
 describe("day 7: part one", () => {
   const testData = [
@@ -30,22 +30,5 @@ describe("day 7: part one", () => {
     expect(output[0]).toBe("FE");
     expect(output[6]).toBe("CF");
     expect(output).toHaveLength(7);
-  });
-
-  test("create graph", () => {
-    const parsedInput = parseInput(testData);
-    const graph = createGraph(parsedInput);
-
-    expect(graph.getStartVertex().toString()).toBe("C");
-    expect(graph.getEndVertex().toString()).toBe("E");
-  });
-
-  test("output graph in string (test data)", () => {
-    const parsedInput = parseInput(testData);
-    const graph = createGraph(parsedInput);
-    const output = graph.toString();
-    const expected = "CABDFE";
-
-    expect(output).toBe(expected);
   });
 });
--- day-seven/src/solution-pt1.ts
+++ day-seven/src/solution-pt1.ts
@@ -1,7 +1,3 @@
-import Graph from "./Graph.class";
-import GraphVertex from "./GraphVertex.class";
-import GraphEdge from "./GraphEdge.class";
-
 //? again this TYPES can't get string[][]
 export const parseInput = (input: string[]): string[] => {
   const regPattern = new RegExp(/(?<!^)[A-Z]/g);
@@ -9,32 +5,4 @@ export const parseInput = (input: string[]): string[] => {
     const keys = str.match(regPattern);
     return keys === null ? "" : keys.join("");
   });
-};
-
-export const createGraph = (input: string[]): Graph => {
-  const graph = new Graph();
-  const vertices = new Map();
-
-  input.forEach(edgeStr => {
-    const [startVertexStr, endVertexStr] = edgeStr.split("");
-    const startVertex = vertices.get(startVertexStr);
-    const endVertex = vertices.get(endVertexStr);
-
-    if (!startVertex) {
-      const vertex = new GraphVertex(startVertexStr);
-      vertices.set(startVertexStr, vertex);
-      graph.addVertex(vertex);
-    }
-
-    if (!endVertex) {
-      const vertex = new GraphVertex(endVertexStr);
-      vertices.set(endVertexStr, vertex);
-      graph.addVertex(vertex);
-    }
-
-    const edge = new GraphEdge(vertices.get(startVertexStr), vertices.get(endVertexStr));
-    graph.addEdge(edge);
-  });
-
-  return graph;
 };
