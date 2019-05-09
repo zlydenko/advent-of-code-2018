7a480deb41346ad8f0713cb4c48e6aac396576c4
anton zlydenko <anton.zlydenko@gmail.com>
2019-05-09 18:50:21 +0300

    very dirty impl of Graph.toString()

--- day-seven/src/__tests__/Graph.spec.ts
+++ day-seven/src/__tests__/Graph.spec.ts
@@ -3,35 +3,83 @@ import GraphEdge from "../GraphEdge.class";
 import Graph from "../Graph.class";
 
 describe("graph", () => {
-  test("add vertices to graph", () => {
-    const vertexA = new GraphVertex("A");
-    const vertexB = new GraphVertex("B");
-    const vertexC = new GraphVertex("C");
-    const edgeAB = new GraphEdge(vertexA, vertexB);
-    const edgeAC = new GraphEdge(vertexA, vertexC);
+  // test("add vertices to graph", () => {
+  //   const vertexA = new GraphVertex("A");
+  //   const vertexB = new GraphVertex("B");
+  //   const vertexC = new GraphVertex("C");
+  //   const edgeAB = new GraphEdge(vertexA, vertexB);
+  //   const edgeAC = new GraphEdge(vertexA, vertexC);
+  //   const graph = new Graph();
+
+  //   graph
+  //     .addVertex(vertexA)
+  //     .addVertex(vertexB)
+  //     .addVertex(vertexC)
+  //     .addEdge(edgeAB)
+  //     .addEdge(edgeAC);
+
+  //   expect(graph.getVertices()).toHaveLength(3);
+  //   expect(graph.getEdges()).toHaveLength(2);
+  // });
+
+  // test("find end vertex", () => {
+  //   const vertexA = new GraphVertex("A");
+  //   const vertexB = new GraphVertex("B");
+  //   const vertexC = new GraphVertex("C");
+  //   const vertexD = new GraphVertex("D");
+  //   const edgeAB = new GraphEdge(vertexA, vertexB);
+  //   const edgeAC = new GraphEdge(vertexA, vertexC);
+  //   const edgeCD = new GraphEdge(vertexC, vertexD);
+  //   const edgeBD = new GraphEdge(vertexB, vertexD);
+  //   const graph = new Graph();
+
+  //   graph
+  //     .addVertex(vertexA)
+  //     .addVertex(vertexB)
+  //     .addVertex(vertexC)
+  //     .addVertex(vertexD)
+  //     .addEdge(edgeAB)
+  //     .addEdge(edgeAC)
+  //     .addEdge(edgeCD)
+  //     .addEdge(edgeBD);
+
+  //   expect(graph.getEndVertex().toString()).toBe("D");
+  // });
+
+  // test("find start vertex", () => {
+  //   const vertexA = new GraphVertex("A");
+  //   const vertexB = new GraphVertex("B");
+  //   const vertexC = new GraphVertex("C");
+  //   const vertexD = new GraphVertex("D");
+  //   const edgeAB = new GraphEdge(vertexA, vertexB);
+  //   const edgeAC = new GraphEdge(vertexA, vertexC);
+  //   const edgeCD = new GraphEdge(vertexC, vertexD);
+  //   const edgeBD = new GraphEdge(vertexB, vertexD);
+  //   const graph = new Graph();
+
+  //   graph
+  //     .addVertex(vertexA)
+  //     .addVertex(vertexB)
+  //     .addVertex(vertexC)
+  //     .addVertex(vertexD)
+  //     .addEdge(edgeAB)
+  //     .addEdge(edgeAC)
+  //     .addEdge(edgeCD)
+  //     .addEdge(edgeBD);
+
+  //   expect(graph.getStartVertex().toString()).toBe("A");
+  // });
+
+  test("get graph to string", () => {
     const graph = new Graph();
-
-    graph
-      .addVertex(vertexA)
-      .addVertex(vertexB)
-      .addVertex(vertexC)
-      .addEdge(edgeAB)
-      .addEdge(edgeAC);
-
-    expect(graph.getVertices()).toHaveLength(3);
-    expect(graph.getEdges()).toHaveLength(2);
-  });
-
-  test("find end vertex", () => {
     const vertexA = new GraphVertex("A");
     const vertexB = new GraphVertex("B");
     const vertexC = new GraphVertex("C");
     const vertexD = new GraphVertex("D");
     const edgeAB = new GraphEdge(vertexA, vertexB);
-    const edgeAC = new GraphEdge(vertexA, vertexC);
-    const edgeCD = new GraphEdge(vertexC, vertexD);
-    const edgeBD = new GraphEdge(vertexB, vertexD);
-    const graph = new Graph();
+    const edgeAD = new GraphEdge(vertexA, vertexD);
+    const edgeBC = new GraphEdge(vertexB, vertexC);
+    const edgeDC = new GraphEdge(vertexD, vertexC);
 
     graph
       .addVertex(vertexA)
@@ -39,34 +87,10 @@ describe("graph", () => {
       .addVertex(vertexC)
       .addVertex(vertexD)
       .addEdge(edgeAB)
-      .addEdge(edgeAC)
-      .addEdge(edgeCD)
-      .addEdge(edgeBD);
+      .addEdge(edgeAD)
+      .addEdge(edgeBC)
+      .addEdge(edgeDC);
 
-    expect(graph.getEndVertex().toString()).toBe("D");
-  });
-
-  test("find start vertex", () => {
-    const vertexA = new GraphVertex("A");
-    const vertexB = new GraphVertex("B");
-    const vertexC = new GraphVertex("C");
-    const vertexD = new GraphVertex("D");
-    const edgeAB = new GraphEdge(vertexA, vertexB);
-    const edgeAC = new GraphEdge(vertexA, vertexC);
-    const edgeCD = new GraphEdge(vertexC, vertexD);
-    const edgeBD = new GraphEdge(vertexB, vertexD);
-    const graph = new Graph();
-
-    graph
-      .addVertex(vertexA)
-      .addVertex(vertexB)
-      .addVertex(vertexC)
-      .addVertex(vertexD)
-      .addEdge(edgeAB)
-      .addEdge(edgeAC)
-      .addEdge(edgeCD)
-      .addEdge(edgeBD);
-
-    expect(graph.getStartVertex().toString()).toBe("A");
+    expect(graph.toString()).toBe("ABDC");
   });
 });
