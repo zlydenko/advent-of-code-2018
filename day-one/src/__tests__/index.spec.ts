13f28339e36f5939353edafe3767844b683dfa2d
anton <anton.zlydenko@gmail.com>
2019-05-17 16:25:41 +0300

    clear day one custom input loading

--- day-one/src/__tests__/index.spec.ts
+++ day-one/src/__tests__/index.spec.ts
@@ -1,29 +1,23 @@
-import { findDuplicateFrequency } from "../index";
+import { findDuplicateFrequency } from '../index';
 
-describe("find duplicate frequency", () => {
-  test("reaches 0 twice", () => {
-    const testData = ["+1", "-1"];
+describe('find duplicate frequency', () => {
+  test('reaches 0 twice', () => {
+    const testData = ['+1', '-1'];
     expect(findDuplicateFrequency(testData, 0)).toBe(0);
   });
 
-  test("reaches 10 twice", () => {
-    const testData = ["+3", "+3", "+4", "-2", "-4"];
+  test('reaches 10 twice', () => {
+    const testData = ['+3', '+3', '+4', '-2', '-4'];
     expect(findDuplicateFrequency(testData, 0)).toBe(10);
   });
 
-  test("reaches 5 twice", () => {
-    const testData = ["-6", "+3", "+8", "+5", "-6"];
+  test('reaches 5 twice', () => {
+    const testData = ['-6', '+3', '+8', '+5', '-6'];
     expect(findDuplicateFrequency(testData, 0)).toBe(5);
   });
 
-  test("reaches 14 twice", () => {
-    const testData = ["+7", "+7", "-2", "-7", "-4"];
+  test('reaches 14 twice', () => {
+    const testData = ['+7', '+7', '-2', '-7', '-4'];
     expect(findDuplicateFrequency(testData, 0)).toBe(14);
   });
 });
-
-// import huy from "../huy";
-
-// test("test", () => {
-//   expect(huy()).toBe("huy");
-// });
