ccc4a7efd1a82e7c3919ef6587381533ae46c196
anton <anton.zlydenko@gmail.com>
2019-05-23 12:53:46 +0300

    skeleton

--- day-eight/src/__tests__/solution-pt1.spec.ts
+++ day-eight/src/__tests__/solution-pt1.spec.ts
@@ -1,10 +1,17 @@
 import { Tree } from '../solution-pt1';
+import inputLoader from '~root/inputLoader';
 
 describe('day 8, part one', () => {
   const testData = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2];
+  const testData2 = [1, 3, 0, 1, 55, 11, 15, 16];
+  const testData3 = [2, 3, 2, 1, 0, 1, 5, 0, 1, 6, 7, 0, 1, 8, 9, 10, 11];
 
-  test('tree initializer', () => {
-    const tree = new Tree(testData);
+  test('tree', () => {
+    // const treeWithChildren = new Tree([1, 1, 0, 1, 66, 55]);
+    // const treeWithoutChildren = new Tree([0, 3, 55, 66, 22]);
+    const tree = new Tree(testData2);
+
     console.log(tree);
+    // console.log(treeWithoutChildren);
   });
 });
--- day-eight/src/solution-pt1.ts
+++ day-eight/src/solution-pt1.ts
@@ -1,32 +1,140 @@
-export interface TreeI {
-  header: [number, number];
-  childNodes: Array<Node>;
-  metadataEntries: Array<number>;
-}
+import nanoid from 'nanoid';
 
-//? subtree
+type id = string;
 
-export interface NodeI extends TreeI {}
+// export class Tree {
+//   private root: TreeNode | null = null;
 
-export class Tree implements TreeI {
-  header: [number, number];
-  childNodes: Array<Node> = [];
-  metadataEntries: Array<number> = [];
-  data: number[];
+//   setRoot(node: TreeNode) {
+//     this.root = node;
+//   }
 
-  constructor(data: number[]) {
-    const [childNodesQuantity, metadataEntriesQuantity, ...childrenInfo] = data;
-    const metadataEntries = childrenInfo.slice(-metadataEntriesQuantity);
-    const childrenData = childrenInfo.slice(0, -metadataEntriesQuantity);
+//   isRootCreated(): boolean {
+//     return this.root !== null;
+//   }
+// }
 
-    this.header = [childNodesQuantity, metadataEntriesQuantity];
-    this.metadataEntries = metadataEntries;
-    this.data = childrenData;
+// export class TreeNode {}
 
-    if (this.header[0] > 0) this._createChildren();
+// const creatingTreeReducer = (tree: Tree, value: number): Tree => {
+//   return tree;
+// };
+
+// const creatingTree = (data: number[]): Tree => {
+//   return data.reduce(creatingTreeReducer, new Tree());
+// };
+
+const x = [
+  {
+    id: 0,
+    header: [1, 3],
+    children: [1]
+  },
+  {
+    id: 1,
+    header: [0, 1],
+    children: null
+  },
+  {
+    id: 2,
+    header: [0, 1],
+    children: null
+  }
+];
+
+export class Tree {
+  private nodes: Node[] = [];
+  private edges: Edge[] = [];
+
+  nodeById(id: id): Node | null {
+    return this.nodes.find(node => node.id === id) || null;
   }
 
-  private _createChildren(): void {}
+  edgeById(id: id): Edge | null {
+    return this.edges.find(edge => edge.id === id) || null;
+  }
+
+  addNode(n: Node): Node {
+    this.nodes.push(n);
+
+    return n;
+  }
+
+  addEdge(e: Edge): Edge {
+    this.edges.push(e);
+
+    return e;
+  }
+
+  isEmpty(): boolean {
+    return this.nodes.length === 0;
+  }
+
+  // findLastUncompleteNode(): Node {
+  //   for (let i = this.nodes.length - 1; i >= 0; i--) {
+  //     const id = this.nodes[i].id;
+  //     const [childrenCount, metaLength] = this.nodes[i].header;
+  //   }
+  // }
 }
 
-export class Node extends Tree {}
+export class Node {
+  id: id;
+  header: [number, number];
+  meta: number[] = [];
+
+  constructor(x: [number, number]) {
+    this.header = x;
+    this.id = nanoid(10);
+  }
+
+  setMeta(m: number[]): Node {
+    this.meta = m;
+
+    return this;
+  }
+}
+
+export class Edge {
+  id: id;
+  child: id;
+  parent: id;
+
+  constructor(child: id, parent: id) {
+    this.child = child;
+    this.parent = parent;
+    this.id = nanoid(10);
+  }
+}
+
+const tree = new Tree();
+const testData2 = [2, 3, 0, 1, 55, 0, 1, 88, 11, 15, 16];
+
+let idx = 0;
+
+while (idx < testData2.length) {
+  let sliced = 2;
+  const header: [number, number] = [testData2[idx], testData2[idx + 1]];
+
+  if (tree.isEmpty()) {
+    const node = new Node(header);
+
+    if (header[0] === 0) {
+      sliced += header[1];
+      const meta = testData2.slice(idx + 2, header[1]);
+      node.setMeta(meta);
+    }
+
+    tree.addNode(node);
+  } else {
+    //? tree is not empty
+    //todo get related node (with no fulfilled children)
+    //todo create node with header
+    //todo create edge between this nodes
+    //todo check if new node havent children -> set meta
+    //todo check if related node now fulfilled
+    //todo if it is -> set him meta
+  }
+
+  idx += sliced - 1;
+}
