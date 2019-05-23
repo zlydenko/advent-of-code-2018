08a0787eee744f7fb7dbc77b2efb332d32a9eb6f
anton <anton.zlydenko@gmail.com>
2019-05-23 17:49:41 +0300

    test for real data(too slow)

--- day-eight/src/__tests__/solution-pt1.spec.ts
+++ day-eight/src/__tests__/solution-pt1.spec.ts
@@ -6,24 +6,33 @@ describe('day 8, part one', () => {
   const testData2 = [1, 3, 0, 1, 55, 11, 15, 16];
   const testData3 = [2, 3, 2, 1, 0, 1, 5, 0, 1, 6, 7, 0, 1, 8, 9, 10, 11];
 
-  test('check meta sum', () => {
-    const tree = buildTree(testData3);
-    const output = tree.getMetaSum();
+  // test('check meta sum', () => {
+  //   const tree = buildTree(testData3);
+  //   const output = tree.getMetaSum();
 
-    expect(output).toBe(56);
-  });
+  //   expect(output).toBe(56);
+  // });
 
-  test('check meta sum', () => {
-    const tree = buildTree(testData2);
-    const output = tree.getMetaSum();
+  // test('check meta sum', () => {
+  //   const tree = buildTree(testData2);
+  //   const output = tree.getMetaSum();
 
-    expect(output).toBe(97);
-  });
+  //   expect(output).toBe(97);
+  // });
 
-  test('check meta sum', () => {
-    const tree = buildTree(testData);
-    const output = tree.getMetaSum();
+  // test('check meta sum', () => {
+  //   const tree = buildTree(testData);
+  //   const output = tree.getMetaSum();
 
-    expect(output).toBe(138);
+  //   expect(output).toBe(138);
+  // });
+
+  test('get meta sum on real data', async () => {
+    const data = await inputLoader('day-eight', (data: string) => data.split(' ').map(v => +v));
+    const tree = buildTree(data);
+    console.log(tree);
+
+    // console.log(tree.getMetaSum());
+    // console.log(data);
   });
 });
