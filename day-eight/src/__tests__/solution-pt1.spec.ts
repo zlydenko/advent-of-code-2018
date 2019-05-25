f071eeb9b05cc0e68db60b0ddc9918f3ca207c87
anton zlydenko <anton.zlydenko@gmail.com>
2019-05-26 01:50:57 +0300

    node

--- day-eight/src/__tests__/solution-pt1.spec.ts
+++ day-eight/src/__tests__/solution-pt1.spec.ts
@@ -1,26 +1,37 @@
-import { Tree } from '../solution-pt1';
-import inputLoader from '~root/inputLoader';
+import { Node } from "../solution-pt1";
+import inputLoader from "~root/inputLoader";
 
-describe('day 8, part one', () => {
-  const testData = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2];
-  const testData2 = [1, 3, 0, 1, 55, 11, 15, 16];
-  const testData3 = [2, 3, 2, 1, 0, 1, 5, 0, 1, 6, 7, 0, 1, 8, 9, 10, 11];
+describe("day 8, part one", () => {
+  const testData = [
+    [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2],
+    [1, 3, 0, 1, 55, 11, 15, 16],
+    [2, 3, 2, 1, 0, 1, 5, 0, 1, 6, 7, 0, 1, 8, 9, 10, 11]
+  ];
 
-  test('check meta sum', () => {
-    const tree = new Tree(testData3);
-    // const metaSum = tree.getMetaSum();
+  test("build simple node", () => {
+    const header: [number, number] = [1, 3];
+    const node = new Node(header);
 
-    tree.showNodes();
-
-    // expect(metaSum).toBe(56);
+    expect(node).toBeInstanceOf(Node);
+    expect(node.getChildrenCount()).toBe(header[0]);
+    expect(node.getMetadataLength()).toBe(header[1]);
   });
 
-  // test('get meta sum on real data', async () => {
-  //   const data = await inputLoader('day-eight', (data: string) => data.split(' ').map(v => +v));
-  //   const tree = buildTree(data);
-  //   console.log(tree);
+  test("check node is fulfilled", () => {
+    const header: [number, number] = [1, 2];
+    const childHeader: [number, number] = [0, 3];
+    const nodeMeta = [1, 2];
+    const childMeta = [1, 2, 3];
+    const node = new Node(header);
+    const childNode = new Node(childHeader);
 
-  //   // console.log(tree.getMetaSum());
-  //   // console.log(data);
-  // });
+    expect(node.isFulfilled).toBe(false);
+    expect(childNode.isFulfilled).toBe(false);
+    childNode.setMeta(childMeta);
+    expect(childNode.isFulfilled).toBe(true);
+    node.setMeta(nodeMeta);
+    expect(node.isFulfilled).toBe(false);
+    node.addChild(childNode);
+    expect(node.isFulfilled).toBe(true);
+  });
 });
