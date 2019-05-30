13e06f5f50df9186785408175969e2d274644c52
anton <anton.zlydenko@gmail.com>
2019-05-30 12:33:48 +0300

    failed, unefficient data structure

--- day-nine/src/__tests__/solution-pt2.spec.ts
+++ day-nine/src/__tests__/solution-pt2.spec.ts
@@ -1,12 +1,79 @@
 import MarbleGame from '../solution-pt2';
+import { MarbleCircle } from '../solution-pt1';
+
+//? 100.000 marbles -> 42s (prev impl)
 
 describe('day 9, part 2', () => {
-  test('test', () => {
-    const game = new MarbleGame(9, 25);
-    game.play();
+  test('example game', () => {
+    const game = new MarbleGame(10, 1618);
+    const highScore = game.getHighScore();
 
-    //? 100.000 marbles -> 42s
+    expect(highScore).toBe(8317);
 
-    // console.log(game.getOutput());
+    //? 16ms
   });
+
+  test('example game', () => {
+    const game = new MarbleGame(13, 7999);
+    const highScore = game.getHighScore();
+
+    expect(highScore).toBe(146373);
+
+    //? 207ms
+  });
+
+  test('example game', () => {
+    const game = new MarbleGame(17, 1104);
+    const highScore = game.getHighScore();
+
+    expect(highScore).toBe(2764);
+
+    //? 15ms
+  });
+
+  test('example game', () => {
+    const game = new MarbleGame(21, 6111);
+    const highScore = game.getHighScore();
+
+    expect(highScore).toBe(54718);
+
+    //? 150ms
+  });
+
+  test('example game', () => {
+    const game = new MarbleGame(30, 5807);
+    const highScore = game.getHighScore();
+
+    expect(highScore).toBe(37305);
+
+    //? 136ms
+  });
+
+  test('part 1 solution (new algo)', () => {
+    const game = new MarbleGame(405, 71700);
+    const highScore = game.getHighScore();
+
+    expect(highScore).toBe(428690);
+
+    //? 21479ms
+  });
+
+  test('part 1 solution (prev algo)', () => {
+    const [playersCount, lastMarble]: number[] = [405, 71700];
+    const game = new MarbleCircle(playersCount);
+    game.makeTurns(lastMarble);
+    const highScore = game.getWinnerScore();
+
+    expect(highScore).toBe(428690);
+
+    //? 47140ms
+  });
+
+  //! it took >15min (i stopped process)
+  //test('part two', () => {
+  //  const lastMarble = 71700;
+  // const largerValue = lastMarble * 10;
+  // const game = new MarbleGame(405, largerValue);
+  // const highScore = game.getHighScore();
+  // });
 });
--- day-nine/src/solution-pt2.ts
+++ day-nine/src/solution-pt2.ts
@@ -16,6 +16,8 @@ export default class MarbleGame {
     this.currentMarble = 0;
     this.output = new LinkedList<number>(0);
     this.lastMarble = lastMarble;
+
+    this.play();
   }
 
   private _emptyScore(players: number): Map<number, number> {
@@ -30,19 +32,62 @@ export default class MarbleGame {
       }, new Map());
   }
 
-  play() {
-    while (this.currentMarble !== this.lastMarble) {
+  private _saveScore(playerId: number, deletedMarble: number, currentMarble: number): void {
+    const currentScore = this.scores.get(playerId) || 0;
+
+    this.scores.set(playerId, currentScore + deletedMarble + currentMarble);
+  }
+
+  private play(): void {
+    while (this.currentMarble <= this.lastMarble) {
       const prevMarble: number = this.currentMarble;
       const currentMarble: number = this.tempMarble === null ? prevMarble + 1 : this.tempMarble;
-      const beforeNode: ListNode<number> = this.output.makeStepsClockwise(prevMarble, 1);
-      const afterNode: ListNode<number> = this.output.makeStepsClockwise(prevMarble, 2);
 
-      this.output.insertBetween(currentMarble, beforeNode, afterNode);
-      this.currentMarble = currentMarble;
+      //? check if currentMarble % 23
+      if (currentMarble % 23 === 0) {
+        //? delete node -7 of prevMarble
+        const deleteNode: ListNode<number> = this.output.makeStepsCounterClockwise(prevMarble, 7);
+        let nextByDeletedNode = deleteNode.next;
+        if (nextByDeletedNode === null && this.output.head !== null) {
+          nextByDeletedNode = this.output.head;
+        } else if (nextByDeletedNode === null && this.output.head === null) {
+          throw new Error('shiet');
+        } else if (nextByDeletedNode !== null) {
+          //? calculate player score
+          this._saveScore(this.currentPlayer, deleteNode.value, currentMarble);
+          //? save to tempMarble currentMarble+1
+          this.tempMarble = currentMarble + 1;
+          //? currentMarble is next by deleted node
+          this.currentMarble = nextByDeletedNode.value;
+          //? delete node
+          this.output.deleteNode(deleteNode);
+        }
+      } else {
+        const beforeNode: ListNode<number> = this.output.makeStepsClockwise(prevMarble, 1);
+        const afterNode: ListNode<number> = this.output.makeStepsClockwise(prevMarble, 2);
+
+        this.output.insertBetween(currentMarble, beforeNode, afterNode);
+        this.currentMarble = currentMarble;
+        if (this.tempMarble !== null) this.tempMarble = null;
+      }
+
+      if (this.tempMarble !== null && this.tempMarble > this.lastMarble) {
+        break;
+      }
+
+      this.currentPlayer = this.currentPlayer === this.playersCount ? 1 : this.currentPlayer + 1;
     }
   }
 
-  getOutput() {
-    return this.output.toString();
+  getOutput(): number[] {
+    return this.output.toArray();
+  }
+
+  getScores(): Map<number, number> {
+    return this.scores;
+  }
+
+  getHighScore(): number {
+    return Array.from(this.scores).reduce((highestScore, currentPlayerState) => (currentPlayerState[1] < highestScore ? highestScore : currentPlayerState[1]), 0);
   }
 }
